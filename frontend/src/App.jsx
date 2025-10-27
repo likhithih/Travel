c42986ad8632e65f52b4f70d3924a0d3830a4ef1
likhithih <likhithih2003@gmail.com>
2025-10-27 15:55:49 +0530

    destination page integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import ProtectedRoute from './Compoents/ProtectedRoute'
 import 'react-toastify/dist/ReactToastify.css';
 import Contact from './pages/Contact'
+import Destination from './pages/Destination'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path='/destination' element={<Destination />} />
       </Routes>
     </>
   )
