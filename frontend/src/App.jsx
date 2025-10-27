8fecc28414273adfd9718e35552d152138730206
likhithih <likhithih2003@gmail.com>
2025-10-27 16:04:52 +0530

    altered the changes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,53 +2,30 @@ import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import LandingPage from "./pages/LandingPage";
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import ProtectedRoute from "./Compoents/ProtectedRoute";
-import "react-toastify/dist/ReactToastify.css";
 import Contact from "./pages/Contact";
 import Booking from "./Compoents/Booking";
 import PreBooking from "./Compoents/PreBooking";
+import ProtectedRoute from "./Compoents/ProtectedRoute";
+import { Route, Routes } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Destination from "./pages/Destination";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/contact"
-          element={
-            <ProtectedRoute>
-              <Contact />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/booking"
-          element={
-            <ProtectedRoute>
-              <Booking />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/pre-booking"
-          element={
-            <ProtectedRoute>
-              <PreBooking />
-            </ProtectedRoute>
-          }
-        />
+
+        {/* Protected Routes */}
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/contact" element={<ProtectedRoute><Contact /></ProtectedRoute>} />
+        <Route path="/booking" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
+        <Route path="/pre-booking" element={<ProtectedRoute><PreBooking /></ProtectedRoute>} />
+        <Route path="/destination" element={<ProtectedRoute><Destination /></ProtectedRoute>} />
       </Routes>
     </>
   );
